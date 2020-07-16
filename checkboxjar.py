37ac1d6734467da02597eb16306c806ec0d06424
Alex Harker <65256748+hackerharker@users.noreply.github.com>
2020-07-16 15:22:06 -0700

    Add files via upload

---
+++ checkboxjar.py
@@ -0,0 +1,28 @@
+# import the library
+from appJar import gui
+
+# create a GUI variable called app
+app = gui()
+
+app.addCheckBox("Apples")
+app.addCheckBox("Pears")
+app.addCheckBox("Oranges")
+app.addCheckBox("Kiwis")
+
+app.addButton('Done', app.stop)
+
+app.go()
+
+print(app.getAllCheckBoxes())
+
+
+Name: dskjdslf
+Pas: slkds
+
+select plants
+pop up window()
+
+making chart.
+pop up chart()
+
+
